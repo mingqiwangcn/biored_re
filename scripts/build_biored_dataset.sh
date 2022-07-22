9ab84da60dc1a28662de0a48ff97cc35299c7ae4
potinglai <potinglai@gmail.com>
2022-07-22 15:37:51 -0400

    init

---
+++ scripts/build_biored_dataset.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo 'Downloading the dataset'
+curl -k -o BIORED.zip https://ftp.ncbi.nlm.nih.gov/pub/lu/BioRED/BIORED.zip
+echo 'Unzip the dataset'
+mkdir -p datasets/biored
+unzip BIORED.zip -d datasets/biored
+rm BIORED.zip
+echo 'Installing Sci spacy en_core_sci_md model'
+pip install https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.5.0/en_core_sci_md-0.5.0.tar.gz
+echo 'Converting the dataset into BioRED-RE input format'
+python src/dataset_format_converter/convert_pubtator_2_bert.py
